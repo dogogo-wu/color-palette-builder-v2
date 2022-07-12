cf0c4c58d9cac4b0ab57e4c9c7476fd7c3e58c66
dogogo-wu <dog9zero0@gmail.com>
2022-07-12 17:54:31 +0800

    my init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <div class="myheader text-h4 text-center mb-2 py-5 font-weight-bold">
+        Color Palette Builder
+      </div>
+      <v-container>
+        <div>
+          <!-- <Picker :pickColor="color_1" /> -->
+          <p>Pick a base color!</p>
+          <input
+            class="mypicker"
+            type="color"
+            name="favcolor"
+            v-model="color_1"
+          />
+          <v-text-field
+            v-model="color_1"
+          ></v-text-field>
+          <!-- <div class="mycir rounded-circle" :style="{backgroundColor: color_1}"></div>
+          <v-color-picker
+            dot-size="25"
+            mode="hex"
+            elevation="15"
+            v-model="color_1"
+          ></v-color-picker> -->
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Picker from "./components/Picker.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    color_1: "#ff0000",
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.myheader {
+  background-color: #eee;
+  color: #555;
 }
-</style>
+.mycir {
+  width: 100px;
+  height: 100px;
+}
+.mypicker {
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  border: none;
+  cursor: pointer;
+
+  &::-webkit-color-swatch {
+    border-radius: 50px;
+    border: none;
+  }
+  &::-moz-color-swatch {
+    border-radius: 50px;
+    border: none;
+  }
+}
+</style>>
+
